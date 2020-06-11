b4abd4d1eb4a6872d44e8d48345d6eed6b44d6b3
Bob den Otter <bobdenotter@gmail.com>
2020-06-11 08:51:36 +0200

    Create EarlyChecks.php

---
+++ src/Event/EarlyChecks.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BobdenOtter\ConfigurationNotices\Event;
+
+use BobdenOtter\ConfigurationNotices\Checks;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class EarlyChecks implements EventSubscriberInterface
+{
+    public function handleEvent()
+    {
+//        $checks = new Checks();
+//
+//        $results = $checks->getEarlyCheckResults();
+
+        echo "Early";
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => ['handleEvent', 3000],
+            ConsoleEvents::COMMAND => ['handleEvent', 3000],
+        ];
+    }
+}
