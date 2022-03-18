a1c581d4cd072f395c91dd9b0e72f2e76dcb4794
Bob den Otter <bobdenotter@gmail.com>
2022-03-18 11:11:05 +0100

    Update src/Checks.php

--- src/Checks.php
+++ src/Checks.php
@@ -619,6 +619,7 @@ class Checks
         $projectDir = $this->container->get('kernel')->getprojectDir();
         $doctrine = Yaml::parseFile($projectDir . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'packages' . DIRECTORY_SEPARATOR . 'doctrine.yaml');
 
+        // Ensure it doesn't break, if there are multiple entity managers causing a different configuration structure
         $functions = array_key_exists('dql', $doctrine['doctrine']['orm']) ?
             $doctrine['doctrine']['orm']['dql']['string_functions'] : $doctrine['doctrine']['orm']['entity_managers']['default']['dql']['string_functions'];
 
